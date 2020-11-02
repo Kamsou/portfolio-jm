9112aefc1a7bbf1530510da027aae120ff38779d
Camille <camille.coutens@gmail.com>
2020-11-02 20:15:43 +0100

    FIX: Add meta title

--- plugins/prismic-vue.js
+++ plugins/prismic-vue.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import PrismicVue from 'prismic-vue'
 import linkResolver from "./link-resolver"
-import htmlSerializer from './html-serializer'
 
 Vue.use(PrismicVue, {
-  linkResolver,
-  htmlSerializer
+  linkResolver: linkResolver,
 })
