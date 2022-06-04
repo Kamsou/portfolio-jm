1aa3ad72e38415bf9ebdebf2b0cf2124b43422af
Camille <camille.coutens@gmail.com>
2022-06-04 23:37:42 +0200

    fix: work carousel

--- components/CarouselSlide.vue
+++ components/CarouselSlide.vue
@@ -1,26 +1,11 @@
 <template>
-  <div v-show="test === index" class="carousel">
-    {{ test }}
+  <div v-show="visibleSlide === index" class="carousel">
     <slot />
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    visibleSlide: {
-      type: Array,
-      default: () => []
-    },
-    index: {
-      type: Number,
-      default: 0
-    }
-  },
-  computed: {
-    test () {
-      return this.visibleSlide[1]
-    }
-  }
+  props: ['visibleSlide', 'index']
 }
 </script>
